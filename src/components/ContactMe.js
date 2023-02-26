2a6d48ebe8dafae4fbb4387af05950feed92be84
Andrea Barrera <andrea.barrera7@gmail.com>
2023-02-25 22:55:45 -0500

    Changing fonts

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,8 +7,27 @@ function ContactMe() {
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <MdOutlineMailOutline />
-      <RxLinkedinLogo />
+      <div className="data-containers">
+        <div className="data-container">
+          <MdOutlineMailOutline />
+          <h5>Email</h5>
+          <button>
+            <a href="mailto:andrea.barrera7@gmail.com">Send a message</a>
+          </button>
+        </div>
+        <div className="data-container">
+          <RxLinkedinLogo />
+          <h5>LinkedIn</h5>
+          <button>
+            <a
+              href="https://www.linkedin.com/in/andrea-barrera-j18/"
+              target="blank"
+            >
+              Send a message
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
