8523f319a6cc344ed7cfa6af41b2143128ceff7f
Andrea Barrera <andrea.barrera7@gmail.com>
2023-02-25 23:37:32 -0500

    Creation of Experience component is done

---
+++ src/components/Experience.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../stylesheets/Experience.css";
+
+function Experience(props) {
+  return (
+    <div className="experience-container">
+      <div className="experience-containers">
+        <h2>{props.position}</h2>
+        <h3>{props.enterprise}</h3>
+        <h6>{props.dates}</h6>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
